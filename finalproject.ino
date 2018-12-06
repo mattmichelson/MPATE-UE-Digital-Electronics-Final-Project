e515cdd8e61aa897160e2bb3ebe58e74653140da
mattmichelson <mm9083@nyu.edu>
2018-12-06 10:35:48 -0500

    Update finalproject.ino
    -Mic broke
    -Tempo lags a bit

--- finalproject.ino
+++ finalproject.ino
@@ -1,16 +1,36 @@
 //Matthew Michelson
 //Digital Electronics final project
+#include <Arduino.h>
+#include <TM1637Display.h>
+
+// Module connection pins (Digital Pins)
+#define CLK 2
+#define DIO 3
+
+const uint8_t SEG_DONE[] = {
+  SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,           // d
+  SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,   // O
+  SEG_C | SEG_E | SEG_G,                           // n
+  SEG_A | SEG_D | SEG_E | SEG_F | SEG_G            // E
+};
+
+TM1637Display display(CLK, DIO);
+
+
+
+
 ///these are all the numbered variables
 int ledPins[8] = {12, 11, 10, 9, 8, 7, 6, 5};
 int buttonPins [3] = {30, 31, 32};
 int switchPin = 28;
 int prevChannelButtonPin = 27;
 int nextChannelButtonPin = 26;
-int channelDisplayed = 0;
-int midiNotes[3] = { 60, 62, 64};
+int channelDisplayed = 1;
+int midiNotes[3] = { 36, 38, 44};
 int currentStep = 0;
 unsigned long lastStepTime = 0;
 unsigned long lastRecordTime = 0;
+unsigned long lastPressTime = 0;
 int tempo = 0;
 
 //booleans
@@ -44,12 +64,20 @@ void setup() {
 }
 
 void loop() {
+  //for the display
+  display.setBrightness(0x0f);
+  if (channelDisplayed == 1) {
+    display.showNumberHexEx(0xc1);
+  }
+
+
   //checks to see the button(s) selected
   //changes the channel
   checkButtons();
   //turns on or off the leds
   if (recordOn == true) {
-    plainSequence();
+    //plainSequence();
+    sequenceWithSelectedStep();
     digitalWrite(13, HIGH);
   }
 
@@ -85,46 +113,75 @@ void checkButtons() {
   //reset button
   for (int i = 0; i < 8; i++) {
     if (buttonStates [1] == HIGH && lastButtonStates [1] == LOW) {
-      on[channelDisplayed][i] = false;
+      on[channelDisplayed - 1][i] = false;
     }
   }
 
+
   //this code switches through the 3 channels
-  lastPrevChannelButtonState = prevChannelButtonState;
-  prevChannelButtonState = digitalRead(prevChannelButtonPin);
+  if (millis() > lastPressTime + 200) {
+    lastPrevChannelButtonState = prevChannelButtonState;
+    prevChannelButtonState = digitalRead(prevChannelButtonPin);
 
-  if (prevChannelButtonState == HIGH && lastPrevChannelButtonState == LOW) {
-    //Serial.println("Current channel is:" channelDisplayed);
-    //displayNumberAndText(channelDisplayed);
-    --channelDisplayed;
+    if (prevChannelButtonState == HIGH && lastPrevChannelButtonState == LOW) {
+      //Serial.println("Current channel is:" channelDisplayed);
+
+      --channelDisplayed;
+      if (channelDisplayed < 1) {
+        channelDisplayed = 3;
+      }
+
+      if (channelDisplayed == 1) {
+        display.showNumberHexEx(0xc1);
+      }
+
+      if (channelDisplayed == 2) {
+        display.showNumberHexEx(0xc2);
+      }
+      if (channelDisplayed == 3) {
+        display.showNumberHexEx(0xc3);
+      }
+      lastPressTime = millis();
+    }
   }
 
-  if (channelDisplayed < 0) {
-    channelDisplayed = 2;
+
+  if (millis() > lastPressTime + 200) {
+    lastNextChannelButtonState = nextChannelButtonState;
+    nextChannelButtonState = digitalRead(nextChannelButtonPin);
+
+    if (nextChannelButtonState == HIGH && lastNextChannelButtonState == LOW) {
+      channelDisplayed++;
+      if (channelDisplayed > 3) {
+        channelDisplayed = 1;
+      }
+      //Serial.println("Current channel is:"channelDisplayed);
+
+      if (channelDisplayed == 1) {
+        display.showNumberHexEx(0xc1);
+      }
+
+      if (channelDisplayed == 2) {
+        display.showNumberHexEx(0xc2);
+      }
+      if (channelDisplayed == 3) {
+        display.showNumberHexEx(0xc3);
+      }
+      lastPressTime = millis();
+    }
   }
 
-  lastNextChannelButtonState = nextChannelButtonState;
-  nextChannelButtonState = digitalRead(nextChannelButtonPin);
 
-  if (nextChannelButtonState == HIGH && lastNextChannelButtonState == LOW) {
-    channelDisplayed++;
-    //Serial.println("Current channel is:"channelDisplayed);
-    //displayNumberAndText(channelDisplayed);
-  }
-
-  if (channelDisplayed > 2) {
-    channelDisplayed = 0;
-  }
 }
 
 void plainSequence() {
-  //step through the LEDs in a different array
+  //detect mic input
 
-  if (millis() > lastRecordTime + 100) {   //if its time to go to the next step...
+  if (millis() > lastRecordTime + 50) {   //if its time to go to the next step...
     //mic button
     if (buttonStates [0] == HIGH && lastButtonStates [0] == LOW) {
-      if (on[channelDisplayed][currentStep] == false) {
-        on[channelDisplayed][currentStep] = true;
+      if (on[channelDisplayed - 1][currentStep] == false) {
+        on[channelDisplayed - 1][currentStep] = true;
         lastRecordTime = millis();
       }
     }
@@ -139,7 +196,7 @@ void plainSequence() {
     if (millis() > lastStepTime + tempo) {   //if its time to go to the next step...
       currentStep = currentStep + 1;         //increment to the next step
       Serial.println(currentStep);
-      if (currentStep > 7) {
+      if (currentStep > 8) {
         currentStep = 0;
       }
 
@@ -163,8 +220,8 @@ void plainSequence() {
     if (millis() > lastStepTime + tempo) {   //if its time to go to the next step...
       //mic button
       if (buttonStates [0] == HIGH && lastButtonStates [0] == LOW) {
-        if (on[channelDisplayed][currentStep] == false) {
-          on[channelDisplayed][currentStep] = true;
+        if (on[channelDisplayed - 1][currentStep] == false) {
+          on[channelDisplayed - 1][currentStep] = true;
         }
       }
 
@@ -207,7 +264,7 @@ void midiSequence() {
         currentStep = 0;
       }
 
-      for (int i = 0; i < 8; i++) {
+      for (int i = 0; i < 3; i++) {
         if (on[i][currentStep] == true) {
           usbMIDI.sendNoteOn(midiNotes[i], 127, 1); //send midi note on
           usbMIDI.sendNoteOff(midiNotes[i], 0, 1); //send midi note off
@@ -226,7 +283,7 @@ void midiSequence() {
     if (millis() > lastStepTime + tempo) {   //if its time to go to the next step...
       //digitalWrite(ledPins[currentStep], LOW);  //turn off the current led
 
-      for (int i = 0; i < 2; i++) {
+      for (int i = 0; i < 3; i++) {
         usbMIDI.sendNoteOff(midiNotes[i], 0, 1); //send Midi note off
       }
 
@@ -235,7 +292,7 @@ void midiSequence() {
         currentStep = 7;
       }
 
-      for (int i = 0; i < 2; i++) {
+      for (int i = 0; i < 3 ; i++) {
         if (on[i][currentStep] == true) {
           usbMIDI.sendNoteOn(midiNotes[i], 127, 1); //send midi note on
           usbMIDI.sendNoteOff(midiNotes[i], 0, 1);//send midi note off
@@ -251,19 +308,30 @@ void midiSequence() {
 void setLeds() {
   for (int i = 0; i < 8; i++) {
     //for  the current channel, if the step is selected turn that LED on
-    if (on [channelDisplayed][i] == true || i == currentStep) {
+    if (on [channelDisplayed - 1][i] == true || i == currentStep) {
       digitalWrite(ledPins [i], HIGH);
       //Serial.println(ledPins [i]);
     }
 
     //for  the current channel, if the step is no longer selected, turn that LED off
-    else if (on [channelDisplayed][i] == false || i == currentStep) {
+    else if (on [channelDisplayed - 1][i] == false || i == currentStep) {
       digitalWrite(ledPins [i], LOW);
     }
   }
 }
 
 void sequenceWithSelectedStep() {
+
+  if (millis() > lastRecordTime + 50) {   //if its time to go to the next step...
+    //mic button
+    if (buttonStates [0] == HIGH && lastButtonStates [0] == LOW) {
+      if (on[channelDisplayed - 1][currentStep] == false) {
+        on[channelDisplayed - 1][currentStep] = true;
+        lastRecordTime = millis();
+      }
+    }
+  }
+
   setLeds();
   tempo = analogRead(A14);
 
@@ -289,26 +357,12 @@ void sequenceWithSelectedStep() {
     if (millis() > lastStepTime + tempo) {   //if its time to go to the next step...
       //digitalWrite(ledPins[currentStep], LOW);  //turn off the current led
 
-      currentStep = currentStep - 1;         //increment to the next step
+      currentStep = currentStep - 1;         //decrement to the next step
       if (currentStep < 0) {
         currentStep = 7;
       }
 
+      lastStepTime = millis();               //set lastStepTime to the current time
     }
-    lastStepTime = millis();               //set lastStepTime to the current time
   }
-}
-
-void displayNumberAndText(int number) {
-  char text[] = "channel:";
-  clearDisplay();
-  Serial1.print(text);
-  char displayFormattedNumber[4];
-  sprintf(displayFormattedNumber, "%4d" , number);
-  clearDisplay();
-  Serial.println(displayFormattedNumber);
-}
-
-void clearDisplay() {
-  Serial1.write(0x76); //clear display command
 }
